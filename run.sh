04437ed31d151cf579983e892a53fdd9fc889dad
saulukas <saulius.narkevicius@tieto.com>
2019-02-27 09:20:10 +0200

    run.sh can be started from any directory

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-java -jar target/saga-jar-with-dependencies.jar $*
+#/bin/bash
+
+DIRECTORY=`dirname $0`
+
+SAGA="java -jar $DIRECTORY/utils/saga-2019-02-21.jar"
+PATH_SEPARATOR=$($SAGA unix-win path-separator)
+
+echo "---- setting classpath ------------------------------------------------"
+CLASS_PATH=$(cd $DIRECTORY && mvn -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath")
+echo $CLASS_PATH | tr "$PATH_SEPARATOR" "\n"
+
+echo "---- starting Saga Tools ----------------------------------------------"
+java -cp $CLASS_PATH saga.SagaTools $*
